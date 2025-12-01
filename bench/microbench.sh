fef25eb5fceff0a92beac5c832f594852c3bf091
Tingmao Wang <m@maowtm.org>
2025-12-01 00:09:34 +0000

    Rework benchmarking script
    - Remove "perf trace" and the associated awk filtering script, due to
      perf-trace introducing significant performance impact and adding noise.
    
    - Enhance open-ntimes.c to:
      - Perform warm-up runs
      - Directly measure openat() time and collect stats
      - Produce a fine-grained histogram
      - Output both in JSON
    
    - Enhance microbench.sh to:
      - Support running on a specific CPU core
      - Support varying number of extra Landlock rules to test the ruleset
        lookup performance
    	- In order to do this, we create a fixed number of "dummy" files in a
    	  unrelated directory.
      - Optionally use bpftrace to measure Landlock hook overhead
        - bpftrace also has an overhead, but not as bad as perf-trace.
      - Produce JSON log for which tests were run
    
    - Add a Python script to parse the JSON output, do comparisons, estimate
      Landlock overhead, and plot histograms.
      - In the latest version, Landlock overhead can be estimated by comparing
        the average for with Landlock vs. without Landlock, to not require
        bpftrace.
      - The histogram is also not dependent on bpftrace output anymore due the
        open-ntimes.c being able to output histograms itself.
    
    Signed-off-by: Tingmao Wang <m@maowtm.org>

--- bench/microbench.sh
+++ bench/microbench.sh
@@ -4,24 +4,137 @@
 # cd linux
 # ARCH=x86_64 .../check-linux.sh build_light
 # # Run a VM with this new kernel
-# .../microbench.sh vm0 | .../filter-microbench.awk
+# .../microbench.sh --ssh vm0
 #
 # Copyright © 2025 Mickaël Salaün <mic@digikod.net>.
+# Copyright © 2025 Tingmao Wang <m@maowtm.org>.
 
 set -e -u -o pipefail
 
-# In sync with to filter-microbench.awk
-NUM_ITERATIONS="1000000"
+NUM_ITERATIONS="15000000"
 
 DIRNAME="$(dirname -- "${BASH_SOURCE[0]}")"
 BASENAME="$(basename -- "${BASH_SOURCE[0]}")"
 
-if [[ $# -gt 1 ]]; then
-	echo "usage: ${BASENAME} [ssh-host] | .../filter-microbench.awk" >&2
-	exit 1
-fi
+RUN_ON_CPU=""
+DO_BPFTRACE=0
+SSH_HOST=""
+VERBOSE=0
+NB_TRIALS=3
 
-SSH_HOST="${1:-}"
+DEPTHS_ARR=(0 10 29)
+NB_EXTRA_RULES_ARR=(0 10 1000)
+
+print_usage() {
+	echo "Usage: ${BASENAME} [--cpu <cpu_number>] [--bpftrace] [--ssh <host>]"
+	echo "  --cpu <cpu_number>   Run the benchmark on the specified CPU core."
+	echo "                       (default: 0)"
+	echo "  --bpftrace           Use bpftrace to measure Landlock overhead."
+	echo "  --ssh <host>         Use SSH to run the benchmark on the specified host."
+	echo "  --verbose            Enable verbose output."
+	echo "  --trials             <number>"
+	echo "      Number of trials for each test size (default: ${NB_TRIALS})."
+	echo "      This is probably only necessary for hashtable benchmarks."
+	echo "  --num-iterations <number>"
+	echo "      Number of openat iterations per test (default: ${NUM_ITERATIONS})."
+	echo "  --depths d1,d2,..."
+	echo "      Comma-separated list of directory depths to test.  Max depth is 29."
+	echo "  --extra-rules n1,n2,..."
+	echo "      Comma-separated list of numbers of extra rules to test."
+	exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		--cpu)
+			shift
+			if [[ $# -eq 0 ]]; then
+				print_usage
+			fi
+			RUN_ON_CPU="$1"
+			shift
+			;;
+		--bpftrace)
+			DO_BPFTRACE=1
+			shift
+			;;
+		--ssh)
+			shift
+			if [[ $# -eq 0 ]]; then
+				print_usage
+			fi
+			SSH_HOST="$1"
+			shift
+			;;
+		--verbose)
+			VERBOSE=1
+			shift
+			;;
+		--trials)
+			shift
+			if [[ $# -eq 0 ]]; then
+				print_usage
+			fi
+			NB_TRIALS="$1"
+			shift
+			;;
+		--num-iterations)
+			shift
+			if [[ $# -eq 0 ]]; then
+				print_usage
+			fi
+			NUM_ITERATIONS="$1"
+			shift
+			;;
+		--depths)
+			shift
+			if [[ $# -eq 0 ]]; then
+				print_usage
+			fi
+			IFS=',' read -r -a DEPTHS_ARR <<< "$1"
+			shift
+			if [[ ${#DEPTHS_ARR[@]} -eq 0 ]]; then
+				print_usage
+			fi
+			for depth in "${DEPTHS_ARR[@]}"; do
+				if ! [[ "$depth" =~ ^[0-9]+$ ]] || [[ "$depth" -lt 0 ]] || [[ "$depth" -gt 29 ]]; then
+					echo "ERROR: Invalid depth: $depth" >&2
+					print_usage
+				fi
+			done
+			;;
+		--extra-rules)
+			shift
+			if [[ $# -eq 0 ]]; then
+				print_usage
+			fi
+			IFS=',' read -r -a NB_EXTRA_RULES_ARR <<< "$1"
+			shift
+			if [[ ${#NB_EXTRA_RULES_ARR[@]} -eq 0 ]]; then
+				print_usage
+			fi
+			for nb_extra_rules in "${NB_EXTRA_RULES_ARR[@]}"; do
+				if ! [[ "$nb_extra_rules" =~ ^[0-9]+$ ]] || [[ "$nb_extra_rules" -lt 0 ]]; then
+					echo "ERROR: Invalid number of extra rules: $nb_extra_rules" >&2
+					print_usage
+				fi
+			done
+			;;
+		*)
+			print_usage
+			;;
+	esac
+done
+
+print_verbose() {
+	if [[ $VERBOSE -eq 1 ]]; then
+		echo "[#]" "$@" >&2
+	fi
+}
+
+if [[ -z "$RUN_ON_CPU" ]]; then
+	RUN_ON_CPU=0
+fi
 
 BUILD_DIR=".out-landlock_local-x86_64-gcc"
 
@@ -58,28 +171,91 @@ get_file "${DIRNAME}/run-bench-in-namespace.sh"
 get_file "${BUILD_DIR}/samples/landlock/sandboxer"
 get_file "tools/perf/perf" make -C "tools/perf"
 
+print_test_setup_json() {
+	local do_landlock="$1"
+	local dir_depth="$2"
+	local nb_extra_rules="$3"
+
+	jq -nc \
+		--arg do_landlock "$do_landlock" \
+		--arg dir_depth "$dir_depth" \
+		--arg nb_extra_rules "$nb_extra_rules" \
+		'{
+			landlock: ($do_landlock == "1"),
+			dir_depth: ($dir_depth | tonumber),
+			nb_extra_rules: ($nb_extra_rules | tonumber)
+		}'
+}
+
 run_test() {
-	local d="$1"
-	local sandboxer="${2:-}"
-	local cmd=(env LL_FS_RO=/ LL_FS_RW=/ IN_BENCHMARK_NS=1 unshare --mount -- ./run-bench-in-namespace.sh ./perf trace -s -e openat -- ${sandboxer} ./open-ntimes "${NUM_ITERATIONS}" 0 "$d")
-
-	if [[ -n "${sandboxer}" ]]; then
-		echo -n "[*] with sandbox"
-	else
-		echo -n "[*] without sandbox"
+	local do_landlock="$1"
+	local dir_depth="$2"
+	local nb_extra_rules="$3"
+	local maybe_sandboxer=()
+	if [[ $do_landlock -eq 1 ]]; then
+		maybe_sandboxer=("./sandboxer")
 	fi
-	echo " d=$d"
-
+	local d="$(echo /1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9 | head -c $((dir_depth * 2 + 1)))"
+	local LL_FS_RO=/
+	local LL_FS_RW=/
+	for i in $(seq 1 $nb_extra_rules); do
+		LL_FS_RO+=":/extra_rules/_$i"
+	done
+	local expected_errno=0
+	local maybe_ssh=()
 	if [[ -n "${SSH_HOST}" ]]; then
-		echo "[+] ssh ${SSH_HOST} ${cmd[*]}"
-		ssh "${SSH_HOST}" -- "${cmd[@]}"
-	else
-		echo "[+] ${cmd[*]}"
-		"${cmd[@]}"
+		maybe_ssh=(ssh "$SSH_HOST" --)
+	fi
+	local cmd=(
+		"${maybe_ssh[@]}"
+		env LL_FS_RO="$LL_FS_RO" LL_FS_RW="$LL_FS_RW" IN_BENCHMARK_NS=1 VERBOSE=$VERBOSE unshare --mount --
+		./run-bench-in-namespace.sh taskset -c "${RUN_ON_CPU}" "${maybe_sandboxer[@]}"
+		./open-ntimes "$NUM_ITERATIONS" "$expected_errno" "$d"
+	)
+
+	print_test_setup_json "$do_landlock" "$dir_depth" "$nb_extra_rules"
+	print_verbose "Running command: ${cmd[*]}"
+
+	local bpftrace_pid=""
+	if [[ $DO_BPFTRACE -ne 0 ]]; then
+		local bpftrace_cmd=(
+			bpftrace -f json landlock_overhead.bt $RUN_ON_CPU
+		)
+		print_verbose "Running command: ${bpftrace_cmd[*]}"
+		"${maybe_ssh[@]}" "${bpftrace_cmd[@]}" &
+		bpftrace_pid=$!
+		sleep 1
+	fi
+
+	"${cmd[@]}"
+
+	if [[ -n "$bpftrace_pid" ]]; then
+		kill -INT $bpftrace_pid
+		wait $bpftrace_pid
 	fi
 }
 
-for d in / /1/2/3/4/5/6/7/8/9/ /1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9 /1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9; do
-	run_test "$d" 2>&1
-	run_test "$d" ./sandboxer 2>&1
+curr_count=0
+total=$((NB_TRIALS * ${#DEPTHS_ARR[@]} * (1 + ${#NB_EXTRA_RULES_ARR[@]})))
+
+print_progress() {
+	local percent=$((curr_count * 100 / total))
+	local msg="$1"
+	if [[ $percent -lt 10 ]]; then
+		percent=" $percent"
+	fi
+	echo "[ ${percent}%] $msg" >&2
+}
+
+for trial in $(seq 1 $NB_TRIALS); do
+	for depth in "${DEPTHS_ARR[@]}"; do
+		print_progress "Running trial $trial for no Landlock"
+		run_test 0 $depth 0
+		curr_count=$((curr_count + 1))
+		for nb_extra_rules in "${NB_EXTRA_RULES_ARR[@]}"; do
+			print_progress "Running trial $trial for depth $depth with $nb_extra_rules extra rules"
+			run_test 1 $depth $nb_extra_rules
+			curr_count=$((curr_count + 1))
+		done
+	done
 done
--- bench/open-ntimes.c
+++ bench/open-ntimes.c
@@ -9,31 +9,172 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-	ssize_t ntimes;
+/* Histogram configuration (in nanoseconds) */
+#define HIST_MIN 100
+#define HIST_STEP 10
+#define HIST_MAX 2000
+
+/* Number of histogram buckets - +2 for the open-ended buckets */
+#define HIST_BUCKETS ((((HIST_MAX) - (HIST_MIN)) / (HIST_STEP)) + 2)
+
+static bool has_verbose(void)
+{
+	const char *verbose = getenv("VERBOSE");
+	return verbose && strcmp(verbose, "0") != 0;
+}
+
+struct stats {
+	uint64_t sum;
+	uint64_t sum_of_squares;
+	uint64_t min;
+	uint64_t max;
+	uint64_t count;
+};
+
+static void add_stat(struct stats *stats, uint64_t value)
+{
+	stats->sum += value;
+	stats->sum_of_squares += value * value;
+	stats->count++;
+	if (value < stats->min)
+		stats->min = value;
+	if (value > stats->max)
+		stats->max = value;
+}
+
+static void print_stats(const struct stats *stats, uint64_t ntimes)
+{
+	double mean = (double)stats->sum / ntimes;
+	/* var = E[X^2] - (E[X])^2 */
+	double variance =
+		((double)stats->sum_of_squares / ntimes) - (mean * mean);
+	double stddev = sqrt(variance);
+
+	printf("{\"type\":\"cstats\",\"ntimes\":%lu,\"mean\":%.4f,\"stddev\":%.4f,"
+	       "\"min\":%lu,\"max\":%lu,\"sum_of_squares\":%lu}\n",
+	       ntimes, mean, stddev, stats->min, stats->max,
+	       stats->sum_of_squares);
+}
+
+/*
+ * Get the histogram bucket index for a given value.
+ */
+static int get_hist_bucket(uint64_t value)
+{
+	if (value < HIST_MIN)
+		return 0;
+	else if (value >= HIST_MAX)
+		return HIST_BUCKETS - 1;
+	else
+		return (value - HIST_MIN) / HIST_STEP + 1;
+}
+
+/*
+ * Add a sample to the histogram.
+ */
+static void add_to_histogram(uint64_t *histogram, uint64_t value)
+{
+	int bucket = get_hist_bucket(value);
+	if (bucket >= 0 && bucket < HIST_BUCKETS)
+		histogram[bucket]++;
+}
+
+/*
+ * Print the histogram in JSON format.
+ */
+static void print_histogram(const uint64_t *histogram, const struct stats *stats)
+{
+	printf("{\"type\":\"chist\",\"buckets\":[");
+	bool printed_any = false;
+	for (int i = 0; i < HIST_BUCKETS; i++) {
+		if (printed_any)
+			printf(",");
+		uint64_t this_min, this_max;
+		if (i == 0) {
+			this_min = stats->min;
+			this_max = HIST_MIN - 1;
+		} else if (i == HIST_BUCKETS - 1) {
+			this_min = HIST_MAX;
+			this_max = stats->max;
+		} else {
+			this_min = HIST_MIN + (i - 1) * HIST_STEP;
+			this_max = this_min + HIST_STEP - 1;
+		}
+		if (this_min > this_max)
+			continue;
+		printed_any = true;
+		printf("{\"min\":%lu,\"max\":%lu,\"count\":%lu}",
+		       this_min, this_max, histogram[i]);
+	}
+	printf("]}\n");
+}
+
+int main(int argc, char *argv[])
+{
+	ssize_t ntimes, prepare_ntimes;
 	int err;
 	const char *path;
+	bool verbose = has_verbose();
+	struct stats stats = {
+		.sum = 0,
+		.sum_of_squares = 0,
+		.min = UINT64_MAX,
+		.max = 0,
+		.count = 0,
+	};
+	struct timespec test_start = {}, test_end = {};
+	uint64_t nsecs_total;
+	uint64_t *histogram;
 
 	if (argc != 4)
 		return 1;
 
+	/* Allocate histogram array */
+	histogram = calloc(HIST_BUCKETS, sizeof(uint64_t));
+	if (!histogram) {
+		perror("Failed to allocate histogram");
+		return 1;
+	}
+
 	ntimes = atoi(argv[1]);
-	printf("ntimes: %ld\n", ntimes);
+	if (verbose)
+		printf("[#] ntimes: %ld\n", ntimes);
 	if (ntimes <= 0)
 		return 1;
 
+	prepare_ntimes = ntimes / 5;
+	if (verbose)
+		printf("[#] running open for %ld times to warm up first.\n",
+		       prepare_ntimes);
+
 	err = atoi(argv[2]);
-	printf("expected errno: %ld\n", err);
+	if (verbose)
+		printf("[#] expected errno: %d\n", err);
 
 	path = argv[3];
-	printf("path: %s\n", path);
+	if (verbose)
+		printf("[#] path: %s\n", path);
 
-	for (size_t i = 0; i < ntimes; i++) {
-		int fd = open(path, O_RDONLY);
+	for (size_t i = 0; i < prepare_ntimes + ntimes; i++) {
+		struct timespec start, end;
+		int fd;
+		uint64_t ns_elapsed;
+
+		if (clock_gettime(CLOCK_MONOTONIC, &start))
+			perror("clock_gettime");
+		fd = open(path, O_RDONLY);
+		if (clock_gettime(CLOCK_MONOTONIC, &end))
+			perror("clock_gettime");
+
 		if (fd < 0) {
 			if (err != errno) {
 				perror("Unexpected error");
@@ -46,9 +187,35 @@ int main(int argc, char *argv[]) {
 			}
 			close(fd);
 		}
-		if (i % (ntimes / 10) == 0) {
-			printf("i: %ld\n", i);
+
+		ns_elapsed = (end.tv_sec - start.tv_sec) * 1000000000ULL +
+			     (end.tv_nsec - start.tv_nsec);
+
+		if (verbose && i == prepare_ntimes) {
+			if (clock_gettime(CLOCK_MONOTONIC, &test_start))
+				perror("clock_gettime");
+			printf("[#] Done warming up.\n");
+		}
+		if (i >= prepare_ntimes) {
+			add_stat(&stats, ns_elapsed);
+			add_to_histogram(histogram, ns_elapsed);
 		}
 	}
+
+	if (verbose) {
+		if (clock_gettime(CLOCK_MONOTONIC, &test_end))
+			perror("clock_gettime");
+
+		nsecs_total =
+			(test_end.tv_sec - test_start.tv_sec) * 1000000000ULL +
+			(test_end.tv_nsec - test_start.tv_nsec);
+
+		printf("[#] Total time: %lu ns => %.4f avg\n", nsecs_total,
+		       (double)nsecs_total / ntimes);
+	}
+
+	print_stats(&stats, stats.count);
+	print_histogram(histogram, &stats);
+	free(histogram);
 	return 0;
 }
